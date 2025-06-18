8b058bbaabe579b5135610c5498822b95c11049a
Shahad Ali Aldawsari <shahodah.ali@gmail.com>
2025-06-18 15:03:30 +0300

    Updated RecipeApi with headers

--- app/src/main/java/com/example/recipesapp/dataLayer/remote/RecipeApi.kt
+++ app/src/main/java/com/example/recipesapp/dataLayer/remote/RecipeApi.kt
@@ -3,17 +3,19 @@ package com.example.recipesapp.dataLayer.remote
 import com.example.recipesapp.dataLayer.remote.dto.RecipeDto
 import com.example.recipesapp.dataLayer.remote.dto.RecipeSearchResponse
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface RecipeApi {
-
-    @GET("recipe/search")
+    @Headers("Authorization: Token 9c8b06d329136da358c2d00e76946b0111ce2c48")
+    @GET("api/recipe/search")
     suspend fun searchRecipeName(
         @Query("query")query: String,
         @Query("page") page: Int =1
     ): RecipeSearchResponse
 
-    @GET("recipe/get")
+    @Headers("Authorization: Token 9c8b06d329136da358c2d00e76946b0111ce2c48")
+    @GET("api/recipe/get")
     suspend fun getRecipeByID(
         @Query("id")id:Int
     ): RecipeDto
